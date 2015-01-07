6617cccc55d7ab85d4d2ea958f7e0262708dd3cf
Danny Smart <dannysmart@silktide.com>
2015-01-07 18:05:47 +0000

    use static instead of self (for inheritance)

--- src/Compiler/AbstractPuzzleConfig.php
+++ src/Compiler/AbstractPuzzleConfig.php
@@ -3,7 +3,6 @@
  * Silktide Nibbler. Copyright 2013-2014 Silktide Ltd. All Rights Reserved.
  */
 namespace Downsider\PuzzleDI\Compiler;
-use Downsider\PuzzleDI\Exception\ConfigurationException;
 
 /**
  *
@@ -14,11 +13,11 @@ abstract class AbstractPuzzleConfig
     protected static $configList = [];
 
     public static function getConfigPaths($key) {
-        if (empty(self::$configList[$key])) {
+        if (empty(static::$configList[$key])) {
             // return an empty array
             return [];
         }
-        return self::$configList[$key];
+        return static::$configList[$key];
     }
 
 } 
