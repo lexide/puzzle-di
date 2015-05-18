f4a118a93cae2b26b5e17fe28a0c2d86a64569fb
Danny Smart <dannysmart@silktide.com>
2015-05-18 12:55:52 +0100

    Create the PuzzleConfig class even if no repos are configured for Puzzle-DI  - added comments for readability

--- src/Controller/ScriptController.php
+++ src/Controller/ScriptController.php
@@ -2,9 +2,6 @@
 namespace Downsider\PuzzleDI\Controller;
 use Composer\Script\Event;
 use Downsider\PuzzleDI\Compiler\PuzzleClassCompiler;
-use Downsider\PuzzleDI\Exception\ConfigurationException;
-use Composer\Repository\RepositoryInterface;
-use Composer\Package\Package;
 use Downsider\PuzzleDI\Helper\PuzzleDataCollector;
 
 /**
@@ -18,6 +15,7 @@ class ScriptController
         $output = $event->getIO();
         $composer = $event->getComposer();
 
+        // find repos that are configured to use Puzzle-DI
         $dataCollector = new PuzzleDataCollector($composer->getInstallationManager());
         $repo = $composer->getRepositoryManager()->getLocalRepository();
 
@@ -26,20 +24,24 @@ class ScriptController
         if (empty($data)) {
             // don't throw an exception in this case as we may not have installed any modules that use Puzzle DI
             $output->write("No installed modules are configured for use with Puzzle DI");
-            return;
+            // we still need to create the PuzzleConfig class, so don't end the script here
         }
 
         $compiler = new PuzzleClassCompiler();
 
+        // find the absolute path to the parent package's target directory
         $package = $composer->getPackage();
         $autoload = $package->getAutoload();
         $appNamespace = "";
         $appSourceDir = realpath($composer->getConfig()->get("vendor-dir") . "/../") . "/" . $package->getTargetDir();
         $autoloadType = isset($autoload["psr-4"])? "psr-4": (isset($autoload["psr-0"])? "psr-0": null);
+        // if we're using PSR-x, use the target directory defined for that
         if (!empty($autoloadType)) {
             $appNamespace = key($autoload["psr-4"]);
             $appSourceDir .= current($autoload["psr-4"]);
         }
+
+        // generate the PuzzleConfig class
         $output->write("<info>PuzzleDI: Compiling</info> <comment>{$appNamespace}PuzzleConfig</comment> <info>to</info> <comment>$appSourceDir/PuzzleConfig.php</comment>");
         $compiler->compile($data, $appNamespace, $appSourceDir);
     }
