5c409dcf8fc38f9da5c7163aee1823acc7ad3bd4
Danny Smart <dannysmart@silktide.com>
2016-01-25 14:34:53 +0000

    Where a project has more than one namespace defined, allow the namespace to be set manually in puzzle-DI's "extra" config

--- src/Controller/ScriptController.php
+++ src/Controller/ScriptController.php
@@ -8,6 +8,7 @@ namespace Downsider\PuzzleDI\Controller;
 
 use Composer\Script\Event;
 use Downsider\PuzzleDI\Compiler\PuzzleClassCompiler;
+use Downsider\PuzzleDI\Exception\ConfigurationException;
 use Downsider\PuzzleDI\Helper\PuzzleDataCollector;
 
 class ScriptController
@@ -32,8 +33,12 @@ class ScriptController
 
         $compiler = new PuzzleClassCompiler();
 
-        // find the path to the parent package's target directory
+        // Load the packages extra info so we can see if PuzzleDI needs additional information
         $package = $composer->getPackage();
+        $extra = $package->getExtra();
+        $puzzleConfig = empty($extra["puzzle-di"])? []: $extra["puzzle-di"];
+
+        // find the path to the parent package's target directory
         $appNamespace = "";
         $appRootDir = getcwd(); // the cwd will always be the directory that the composer.json file is in
         $appSourceDir = $appRootDir . (empty($package->getTargetDir())? "": "/" . $package->getTargetDir());
@@ -42,21 +47,26 @@ class ScriptController
         $autoload = $package->getAutoload();
         $autoloadType = isset($autoload["psr-4"])? "psr-4": (isset($autoload["psr-0"])? "psr-0": null);
         if (!empty($autoloadType)) {
-            // main project namespace MUST be the first entry
-            // TODO: fix this so we intelligently select the namespace and source dir from all possible autoload entries
-            $appNamespace = key($autoload[$autoloadType]);
-            $autoloadSourceDir = current($autoload[$autoloadType]);
+            // if the main project namespace is not the first autoload entry, it needs to be set in puzzle-di config
+            $appNamespace = !empty($puzzleConfig["namespace"])
+                ? $puzzleConfig["namespace"]
+                : key($autoload[$autoloadType]);
+
+            if (empty($autoload[$autoloadType][$appNamespace])) {
+                throw new ConfigurationException("Cannot compile PuzzleConfig. The application namespace '$appNamespace' is not registered with composer");
+            }
+
+            $autoloadSourceDir = $autoload[$autoloadType][$appNamespace];
             if (!empty($autoloadSourceDir)) {
                 $appSourceDir .= "/" . $autoloadSourceDir;
             }
         }
 
-        // Load the packages extra info so we can see if PuzzleDI needs to use absolute paths
-        $extra = $composer->getPackage()->getExtra();
+
         // As composer uses absolute paths when installing modules, we have to do extra work to get relative URLs
         // The path mask will remove itself from any path that starts with the mask
         // e.g. a path mask of '/home/puzzle-di/' will turn '/home/puzzle-di/app/config.yml' into 'app/config.yml'
-        $pathMask = !empty($extra["puzzle-di"]["absolute-paths"])? "": $appRootDir . "/";
+        $pathMask = !empty($puzzleConfig["absolute-paths"])? "": $appRootDir . "/";
 
         // generate the PuzzleConfig class
         $output->write("<info>PuzzleDI: Compiling</info> <comment>{$appNamespace}PuzzleConfig</comment> <info>to</info> <comment>$appSourceDir/PuzzleConfig.php</comment>");
