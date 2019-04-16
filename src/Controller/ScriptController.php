7d37e842fcd621a3f84e91a0ff0f351779a4c057
Danny Smart <danny.smart@lexisnexisrisk.com>
2019-04-16 16:01:43 +0100

    Use the PHP `DIRECTORY_SEPARATOR` constant instead of assuming the correct character

--- src/Controller/ScriptController.php
+++ src/Controller/ScriptController.php
@@ -59,7 +59,7 @@ class ScriptController
         // find the path to the parent package's target directory
         $appNamespace = "";
         $appRootDir = getcwd(); // the cwd will always be the directory that the composer.json file is in
-        $appSourceDir = $appRootDir . (empty($package->getTargetDir())? "": "/" . $package->getTargetDir());
+        $appSourceDir = $appRootDir . (empty($package->getTargetDir())? "": DIRECTORY_SEPARATOR . $package->getTargetDir());
 
         // if we're using PSR-x, use the target directory defined for that
         $autoload = $package->getAutoload();
@@ -76,7 +76,7 @@ class ScriptController
 
             $autoloadSourceDir = $autoload[$autoloadType][$appNamespace];
             if (!empty($autoloadSourceDir)) {
-                $appSourceDir .= "/" . $autoloadSourceDir;
+                $appSourceDir = realpath($appSourceDir . DIRECTORY_SEPARATOR . $autoloadSourceDir);
             }
         }
 
@@ -84,7 +84,7 @@ class ScriptController
         // As composer uses absolute paths when installing modules, we have to do extra work to get relative URLs
         // The path mask will remove itself from any path that starts with the mask
         // e.g. a path mask of '/home/puzzle-di/' will turn '/home/puzzle-di/app/config.yml' into 'app/config.yml'
-        $pathMask = !empty($puzzleConfig["absolute-paths"])? "": $appRootDir . "/";
+        $pathMask = !empty($puzzleConfig["absolute-paths"])? "": $appRootDir . DIRECTORY_SEPARATOR;
 
         // generate the PuzzleConfig class
         $output->write("<info>PuzzleDI: Compiling</info> <comment>{$appNamespace}PuzzleConfig</comment> <info>to</info> <comment>$appSourceDir/PuzzleConfig.php</comment>");
